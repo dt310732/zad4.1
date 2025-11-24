6a53911209d2d8fb722cd71e81044e7f46658005
dt31072 <dt31072@zpsb.pl>
2025-11-24 19:30:30 +0100

    created basic update method

--- zad4.1.py
+++ zad4.1.py
@@ -19,7 +19,20 @@ class Time:
             s = str(self.seconds)
         
         return h + ":" + m + ":" + s
-    
+    def update(self,unit,value):
+        match unit:
+            case 'h':
+                self.hours += value
+            case 'm':
+                self.minutes += value
+            case 's':
+                self.seconds += value
+            case _:
+                print('Wrong type!')
+
+
 
 obj1 = Time(9,9,9)
+print(obj1.display())
+obj1.update('h', 1)
 print(obj1.display())
